43bd6e664282c7f686e1920519834feea721a9d9
tohanov <no>
2021-11-24 23:54:37 +0200

    added a test engine for test automation

---
+++ testEng.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+import subprocess
+import os
+import difflib
+import shutil
+
+scenariosPath = 'tests/scenarios'
+outputsPath = 'tests/outputs'
+sceneFileExtension = '.scene'
+sceneSeparatorPrefix = '\n'
+sceneSeparator = '*****\n'
+sceneSeparatorSuffix = '\n'
+# passedOutputExtension = ".pass"
+failedOutputExtension = ".FAIL.html"
+command = ['bin/studio', 'ExampleInput.txt']
+configFilePath = 'ExampleInput.txt'
+
+
+usage = "\n\033[1;32mUsage:\n\n[*]\033[0m Place the script in the project's dir. Make sure there exists a config file '" + configFilePath + "'. Make sure there is a '" + scenariosPath + "' directory. Each scenario should be a text file of commands, and expected output; scenario files should have the extension '"\
++ sceneFileExtension +\
+"'. Last command in each scenario should be 'closeall'.\n"\
+"\n\n\033[1;32m[*]\033[0m The script is going to mention which scenarios failed. For the failed scenarios, the differences between the expected output and the actual one will be dumped as .html files in the " + outputsPath + " dir.\n"\
+"\n\n\033[1;32m[*]\033[0m format of a .scene file:\n"\
+"\t<series of commands, ending with 'closeall'>\n"\
+"\t<blank line>\n"\
+"\t"+sceneSeparator+\
+"\t<blank line>\n"\
+"\t<expected output>\n"\
+"\n\n\033[1;32m[*]\033[0m To run the script you need python ('sudo apt install python3 -y' on ubuntu/debian systems)\n\n"
+
+
+if not os.path.isdir(scenariosPath):
+	print(usage)
+	exit()
+
+
+if os.path.isdir(outputsPath):
+	shutil.rmtree(outputsPath)
+os.mkdir(outputsPath)
+
+
+for file in os.listdir(scenariosPath):
+		if file.endswith(sceneFileExtension):
+			print("\n[*] running", file)
+			
+			with open(scenariosPath+"/" + file) as f:
+				testText = f.read()
+			
+			inputs,outputs = testText.split(sceneSeparatorPrefix + sceneSeparator + sceneSeparatorSuffix, 1)
+			
+			with subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.STDOUT,stdin=subprocess.PIPE,bufsize=1, universal_newlines=True) as p:
+				stdout,stderr = p.communicate(inputs)
+			
+			if stdout != outputs:
+				print("\033[1;31m[!]\033[0m", file, "\033[1;31mFAILED\033[0m")
+				
+				with open(outputsPath+"/" + file.replace(sceneFileExtension, failedOutputExtension), "w") as f:
+					# for line in difflib.unified_diff(outputs.split("\n"), stdout.split("\n"), fromfile='EXPECTED', tofile='OCCURED'):
+						# print(line)
+					# f.writelines(difflib.unified_diff(outputs.split("\n"), stdout.split("\n"), fromfile='EXPECTED',tofile='OCCURED', lineterm="\n"))
+					# f.write(difflib.HtmlDiff().make_file(outputs.split("\n"), stdout.split("\n"), fromfile='EXPECTED',tofile='OCCURED', lineterm="\n"))
+					
+					f.write(difflib.HtmlDiff().make_file(outputs.split("\n"), stdout.split("\n"),'EXPECTED','OCCURED'))
+			else:
+				print("[+]", file, "passed")
+
+print()
+
